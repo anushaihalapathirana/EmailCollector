82dd2a92f92a0ee600f3ad52d3e1c6cbecc58f57
anu_ihalapathirana <anusha.12@cse.mrt.ac.lk>
2021-02-10 12:25:30 +0530

    sql dump

--- emailcollector.sql
+++ emailcollector.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 10, 2021 at 07:39 AM
+-- Generation Time: Feb 10, 2021 at 07:54 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
